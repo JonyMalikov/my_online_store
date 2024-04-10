f22ae64d33b7bfdbcdd6b30785f140e1f34b66c1
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2024-04-11 00:47:58 +0300

    new protjekt

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """Категория"""
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+    def __str__(self):
+        return self.name
