6372a6a6712b76f22c287261e9af711ce63fb7dd
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2024-04-11 23:57:13 +0300

    create views urls

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -17,6 +18,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        """Абсолютная ссылка на категорию"""
+        return reverse("shop:product_list_by_category", args=[self.slug])
 
 
 class Product(models.Model):
@@ -46,3 +51,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        """Абсолютная ссылка на продукт"""
+        return reverse("shop:product_detail", args=[self.id, self.slug])
