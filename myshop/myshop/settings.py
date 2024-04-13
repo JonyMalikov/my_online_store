7e315b4c56c42aefba64c604d73bdf03ca3c3f09
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2024-04-14 00:09:41 +0300

    add cart

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "shop.apps.ShopConfig",
+    "cart.apps.CartConfig",
 ]
 
 MIDDLEWARE = [
@@ -92,3 +93,5 @@ MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CART_SESSION_ID = "cart"
