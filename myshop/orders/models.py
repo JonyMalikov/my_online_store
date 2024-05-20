73f04a113c023bcfafec1c1cfaae4ef59d4a43c2
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2024-05-20 23:14:49 +0300

    order

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from shop.models import Product
 
 
 class Order(models.Model):
+    """Заказ"""
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -25,3 +28,22 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    """Позиция в заказе"""
+
+    order = models.ForeignKey(
+        Order, related_name="items", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="order_items"
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
