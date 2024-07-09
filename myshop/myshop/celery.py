ae5cc31ef0bfdb325dcbf809195d0c1ccc1fb0cc
Evgeniy_Malikov <malikov.evgeniy@gmail.com>
2024-07-10 00:10:01 +0300

    celery

--- myshop/myshop/celery.py
+++ myshop/myshop/celery.py
@@ -1,7 +1,10 @@
 import os
+
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop_settings')
-app = Celery('myshop')
-app.config_from_object('django.conf:settigs', namespace='CELERY')
+# задать стандартный модуль настроек Django
+# для программы 'celery'.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
+app = Celery("myshop")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
